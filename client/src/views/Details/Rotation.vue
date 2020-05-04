a83a0ceb3c9d641c47b146e43a864ee9b30c7e8a
weiyufan <1873023911@qq.com>
2020-05-04 10:29:41 +0800

    weiyufan 54

---
+++ client/src/views/Details/Rotation.vue
@@ -0,0 +1,73 @@
+<template>
+	<div id="banner">
+		<div class="main">
+			<img class="origin" @mouseover="getin" src="goodsImage" />
+			<div v-show="isin" class="move"></div>
+		</div>
+		<div class="bigimg" v-show="isin"></div>
+	
+	</div>
+</template>
+	
+<script>
+	export default {
+				 name: "Banner",
+				
+				  data() {
+				
+				    return {
+						isin:false
+				    };
+				
+				  },
+				
+				  methods: {
+				getin:function(){
+					this.isin=!this.isin;
+					
+					}
+				
+				  },
+				
+				  mounted() {
+			
+				    },
+
+		props:{
+			goodsImage:{
+				type:String
+			}
+		}
+		}
+	
+	
+	
+</script>
+
+<style>
+	*{
+		padding: 0;
+		margin: 0;
+	}
+	
+	.main {
+		background-color: #7F89C6;
+		width: 40vh;
+		height: 30vh;
+		position: fixed;
+		top: 5vh;
+		left: 3vh;
+		
+		}
+	.move {
+		background-color: #DEDEDE;
+		width:200px;
+		height: 300px;
+		
+		}
+		.bigimg{
+			background-color: #A6C1EE;
+			width: 200px;
+			height:200px;
+			}
+</style>
