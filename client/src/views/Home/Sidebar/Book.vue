167d0eb07be00a58a039db15fa733b64d883583b
2433689944 <2433689944@qq.com>
2020-05-04 15:17:08 +0800

    刘文杰-05-04

--- client/src/views/Home/Sidebar/Book.vue
+++ client/src/views/Home/Sidebar/Book.vue
@@ -1,16 +1,11 @@
 <template>
 	<div>
 		书籍教材
-		<span class="iconfont icon-dianzan" @click="fn" ref="my"></span>
 	</div>
 </template>
 
 <script>
-	export default{
-		methods:{
 
-		}
-	}
 </script>
 
 <style>
